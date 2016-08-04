b085ecd11ade3b68f7fe9cd7f7365dcb2c380325
tony cruz <tonycruz@outlook.com>
2016-08-04 09:44:33 +0100

    Update 04-08-16

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import 'bootstrap';
 
 export class App {
   configureRouter(config, router) {
-    config.title = 'Aurelia';
+    config.title = 'Category and Products';
     config.map([
       { route: ['','welcome'], name: 'welcome', moduleId: './welcome', nav: true, title:'Welcome' },
       { route: "categories", moduleId: "./categories/index", nav: true, title: "categories", name: "categories" }
